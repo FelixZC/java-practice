941dcedf417f3b9aab2764f4e83efd940b202db3
ZC <peng05831@163.com>
2024-03-18 18:07:14 +0800

    git init

---
+++ novel/src/main/java/io/gitee/felixzc/novel/NovelApplication.java
@@ -0,0 +1,13 @@
+package io.gitee.felixzc.novel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelApplication.class, args);
+	}
+
+}
