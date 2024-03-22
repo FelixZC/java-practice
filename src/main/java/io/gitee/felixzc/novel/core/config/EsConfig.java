ea5f86142505e4d82efcded7e949189d12507a7f
ZC <peng05831@163.com>
2024-03-22 11:10:10 +0800

    feat(config):添加配置

---
+++ src/main/java/io/gitee/felixzc/novel/core/config/EsConfig.java
@@ -0,0 +1,20 @@
+package io.gitee.felixzc.novel.core.config;
+
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * elasticsearch 相关配置
+ */
+@Configuration
+public class EsConfig {
+    /**
+     * 解决 ElasticsearchClientConfigurations 修改默认 ObjectMapper 配置的问题
+     */
+    @Bean
+    JacksonJsonpMapper jacksonJsonpMapper() {
+        return new JacksonJsonpMapper();
+    }
+
+}
